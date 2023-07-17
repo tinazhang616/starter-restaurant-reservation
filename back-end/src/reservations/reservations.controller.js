eb52feddd83b94645368812e53ae2f4f115d802f
Tina <tinazhang616@gmail.com>
2023-07-17 15:15:20 -0700

    2nd upload

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,12 +1,21 @@
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary")
+const service = require("./reservations.service")
 /**
  * List handler for reservation resources
  */
+
 async function list(req, res) {
+  const data = await service.list()
+  res.json({data});
+}
+/* create a new reservation */
+async function create(req,res){//need modify
   res.json({
     data: [],
   });
 }
 
 module.exports = {
-  list,
+  list:asyncErrorBoundary(list),
+  create:asyncErrorBoundary(create),
 };
